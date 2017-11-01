a3c103f806a592f381ae69daa590ac7976bce15b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-01 14:49:01 +0000

    Wed Nov 01 14:49:01 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,9 +1,46 @@
 package befaster.solutions;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.counting;
 
 public class Checkout {
-    public static Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
-    }
+	
+	
+	public static int checkout(String skuList)
+	{
+		if(skuList.trim().equals(""))
+			return 0;
+		
+		Map<String, SkuPrice> skuValues = new HashMap<>();
+		skuValues.put("A", SkuPrice.of(50, 3, 130));
+		skuValues.put("B", SkuPrice.of(30, 2, 45));
+		skuValues.put("C", SkuPrice.of(20));
+		skuValues.put("D", SkuPrice.of(15));
+		
+		String[] individualSkus = skuList.split("");
+		if(Arrays.stream(individualSkus).anyMatch(sku -> !skuValues.containsKey(sku)))
+		{
+			return -1;
+		}
+		
+		Map<String, Long> groupedSkus = Arrays.stream(individualSkus).collect(Collectors.groupingBy(s -> s, counting()));
+		return groupedSkus.entrySet()
+				.stream()
+				.map((entry) -> skuValues.get(entry.getKey()).calculatePriceForItemCountOf(entry.getValue()))
+				.collect(Collectors.summingInt(i -> i));
+		
+	}
+	
+
 }
---
+++ src/test/groovy/befaster/solutions/Round1Spec.groovy
@@ -0,0 +1,94 @@
+package befaster.solutions
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class Round1Spec extends Specification{
+	@Unroll
+	def "can purchase a single item"()
+	{
+		expect:
+			computingCheckoutValueOf(sku) == expectedValue
+			
+		where:
+			sku | expectedValue
+			"A" | 50
+			"B" | 30
+			"C" | 20
+			"D" | 15
+	}
+	
+	@Unroll
+	def "can purchase multiple items"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+			
+		where:
+			skuList | expectedValue
+			"A,B" | 80
+			"B,C" | 50
+			"A,B,C,D" | 115
+	}
+	
+	@Unroll
+	def "does not compute and invalid string"()
+	{
+		expect:
+			computingCheckoutValueOf(illegalInput) == -1
+			
+		where:
+			illegalInput | _
+			"A!BB" 		 | _
+			"GARBAGE"    | _
+	}
+	
+	def "returns 0 when there are no more items"()
+	{
+		expect:
+			computingCheckoutValueOf("") == 0
+	}
+	
+	@Unroll
+	def "will apply special offers"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+		
+		where:
+			skuList | expectedValue
+			"A,A,A" | 130
+			"A,A,A,A,A,A" | 260
+			"B,B" | 45
+	}
+	
+	@Unroll
+	def "calculates original value when no special offer apply"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+		
+		where:
+			skuList | expectedValue
+			"C,C" | 40
+			"C,C,C" | 60
+			"C,C,C,C" | 80
+	}
+	
+	@Unroll
+	def "combine special offers and none special offers"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+		
+		where:
+			skuList | expectedValue
+			"A,A,A,B" | 160
+			"A,A,A,A" | 180
+	}
+
+	private static int computingCheckoutValueOf(String skuList)
+	{
+		Checkout.checkout(skuList.split(",").join(""))
+	}
+}
