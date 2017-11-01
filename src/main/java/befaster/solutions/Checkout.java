25dfdbc1fd7bf07c4b89266f6f45f28b99974dd9
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-01 14:53:32 +0000

    Wed Nov 01 14:53:32 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -14,13 +14,7 @@ public class Checkout {
 			return 0;
 		
 		
-		Map<String, SkuPrice> skuValues = new HashMap<>();
-		skuValues.put("A", SkuPrice.of(50));
-		skuValues.put("B", SkuPrice.of(30));
-		skuValues.put("C", SkuPrice.of(20));
-		skuValues.put("D", SkuPrice.of(15));
-		skuValues.put("E", SkuPrice.of(40));
-		skuValues.put("F", SkuPrice.of(10));
+		Map<String, SkuPrice> skuValues = allProducts();
 
 		
 		String[] individualSkus = skuList.split("");
@@ -29,17 +23,63 @@ public class Checkout {
 			return -1;
 		}
 
-		List<SpecialOffer> specialOffers = Arrays.asList(
-				FreeWithOtherPurchaseSpecialOffer.of("E", 2, "B"),
-				BOGOFSpecialOffer.of("F", 2),
-				MultipleItemSpecialOffer.of("A", 5, 200),
-				MultipleItemSpecialOffer.of("A", 3, 130), 
-				MultipleItemSpecialOffer.of("B", 2, 45)
-		);
+		List<SpecialOffer> specialOffers = allSpecialOffers();
 		
 		CheckoutCalculator calculator = new CheckoutCalculator(skuValues, specialOffers);
 		
 		return calculator.calculatePriceFor(Basket.of(individualSkus));		
+	}
+
+	private static List<SpecialOffer> allSpecialOffers() {
+		List<SpecialOffer> specialOffers = Arrays.asList(
+				FreeWithOtherPurchaseSpecialOffer.of("E", 2, "B"),
+				FreeWithOtherPurchaseSpecialOffer.of("N", 3, "M"),
+				FreeWithOtherPurchaseSpecialOffer.of("R", 3, "Q"),
+				BOGOFSpecialOffer.of("F", 2),
+				BOGOFSpecialOffer.of("U", 3),
+				MultipleItemSpecialOffer.of("A", 5, 200),
+				MultipleItemSpecialOffer.of("A", 3, 130), 
+				MultipleItemSpecialOffer.of("B", 2, 45),
+				MultipleItemSpecialOffer.of("H", 10, 80),
+				MultipleItemSpecialOffer.of("H", 5, 45),
+				MultipleItemSpecialOffer.of("K", 2, 150),
+				MultipleItemSpecialOffer.of("P", 5, 200),
+				MultipleItemSpecialOffer.of("Q", 3, 80),
+				MultipleItemSpecialOffer.of("V", 3, 130),
+				MultipleItemSpecialOffer.of("V", 2, 90)
+		);
+		return specialOffers;
+	}
+
+	private static Map<String, SkuPrice> allProducts() {
+		Map<String, SkuPrice> skuValues = new HashMap<>();
+		skuValues.put("A", SkuPrice.of(50));
+		skuValues.put("B", SkuPrice.of(30));
+		skuValues.put("C", SkuPrice.of(20));
+		skuValues.put("D", SkuPrice.of(15));
+		skuValues.put("E", SkuPrice.of(40));
+		skuValues.put("F", SkuPrice.of(10));
+		skuValues.put("G", SkuPrice.of(20));
+		skuValues.put("H", SkuPrice.of(10));
+		skuValues.put("I", SkuPrice.of(35));
+		skuValues.put("J", SkuPrice.of(60));
+		skuValues.put("K", SkuPrice.of(80));
+		skuValues.put("L", SkuPrice.of(90));
+		skuValues.put("M", SkuPrice.of(15));
+		skuValues.put("N", SkuPrice.of(40));
+		skuValues.put("O", SkuPrice.of(10));
+		skuValues.put("P", SkuPrice.of(50));
+		skuValues.put("Q", SkuPrice.of(30));
+		skuValues.put("R", SkuPrice.of(50));
+		skuValues.put("S", SkuPrice.of(30));
+		skuValues.put("T", SkuPrice.of(20));
+		skuValues.put("U", SkuPrice.of(40));
+		skuValues.put("V", SkuPrice.of(50));
+		skuValues.put("W", SkuPrice.of(20));
+		skuValues.put("X", SkuPrice.of(90));
+		skuValues.put("Y", SkuPrice.of(10));
+		skuValues.put("Z", SkuPrice.of(50));
+		return skuValues;
 	}
 	
 
--- src/test/groovy/befaster/solutions/SpecialOfferSpec.groovy
+++ src/test/groovy/befaster/solutions/SpecialOfferSpec.groovy
@@ -69,6 +69,13 @@ class SpecialOfferSpec extends Specification implements CheckoutDsl{
 	}
 	
 	@Unroll
+	def "3N get one M free"()
+	{
+		expect:
+			computingCheckoutValueOf("N,N,N,M") == 120
+	}
+	
+	@Unroll
 	def "get one F free with 2 Others"()
 	{
 		expect:
@@ -81,7 +88,67 @@ class SpecialOfferSpec extends Specification implements CheckoutDsl{
 			"F,F,F,F,F" | 40
 			"F,F,F,F,F,F" | 40
 	}
+	
+	@Unroll
+	def "H is discounted"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+			
+		where:
+			skuList | expectedValue
+			"H,H,H,H,H" | 45
+			"H,H,H,H,H,H,H,H,H,H" | 80
+	}
+	
+	@Unroll
+	def "K is discounted"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+			
+		where:
+			skuList | expectedValue
+			"K,K" | 150
+			"K,K,K,K" | 300
+	}
+	
+	def "5P for 200"()
+	{
+		expect: 
+			computingCheckoutValueOf("P,P,P,P,P") == 200
+	}
+	
+	def "3Q for 80"()
+	{
+		expect:
+			computingCheckoutValueOf("Q,Q,Q") == 80 
+	}
+	
+	def "3R get one Q free"()
+	{
+		expect:
+			computingCheckoutValueOf("R,R,R,Q") == 150
+	}
+	
+	def "3U get one U free"()
+	{
+		expect:
+			computingCheckoutValueOf("U,U,U,U") == 120
+	}
 
+	@Unroll
+	def "2V for 90, 3V for 130"()
+	{
+		expect:
+			computingCheckoutValueOf(skuList) == expectedValue
+			
+		where:
+			skuList | expectedValue
+			"V,V" | 90
+			"V,V,V" | 130
+	}
+	
 	def "random test that failed on the actual run"()
 	{
 		expect:
